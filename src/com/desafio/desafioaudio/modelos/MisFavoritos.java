c9b3ae2ac0ebfaf5b48e82d056ff42cc562eeba0
jarvychavez <jav.chavez94@gmail.com>
2025-04-16 03:31:55 -0600

    finalizar y probar

---
+++ src/com/desafio/desafioaudio/modelos/MisFavoritos.java
@@ -0,0 +1,11 @@
+package com.desafio.desafioaudio.modelos;
+
+public class MisFavoritos {
+    public void adiciones(Audio audio){
+        if(audio.getClasificacion() >=8){
+            System.out.println(audio.getTitulo() + "Es uno de los favoritos del momento");
+        }else{
+            System.out.println(audio.getTitulo() + "Tambien es uno de los favoritos");
+        }
+    }
+}
--- src/com/desafio/desafioaudio/principal/Principal.java
+++ src/com/desafio/desafioaudio/principal/Principal.java
@@ -1,6 +1,7 @@
 package com.desafio.desafioaudio.principal;
 
 import com.desafio.desafioaudio.modelos.Cancion;
+import com.desafio.desafioaudio.modelos.MisFavoritos;
 import com.desafio.desafioaudio.modelos.Podcast;
 
 public class Principal {
@@ -13,25 +14,33 @@ public class Principal {
         miPosdcast.setPresentador("Gabriela Aguilar");
         miPosdcast.setTitulo("Cafe.Tech");
 
+        //cancion
+
         for (int i = 0; i < 100 ; i++) {
             miCancion.meGusta();
 
         }
-        for (int i = 0; i < 5000 ; i++) {
+        for (int i = 0; i < 2000 ; i++) {
             miCancion.reproduce();
         }
 
         System.out.println("Total de reproducciones: " + miCancion.getTotalDeLasReproducciones());
         System.out.println("Total de me gusta: " + miCancion.getTotalDeMeGusta());
 
-        for (int i = 0; i < 200 ; i++) {
+        //Podcast
+
+        for (int i = 0; i < 100 ; i++) {
             miPosdcast.meGusta();
         }
 
-        for (int i = 0; i < 600 ; i++) {
+        for (int i = 0; i < 8000 ; i++) {
             miPosdcast.reproduce();
         }
         System.out.println("Reproducciones en total: " + miPosdcast.getTotalDeLasReproducciones());
         System.out.println("Los me gusta en total: " + miPosdcast.getTotalDeMeGusta());
+
+        MisFavoritos favoritos = new MisFavoritos();
+        favoritos.adiciones(miPosdcast);
+        favoritos.adiciones(miCancion);
     }
 }
